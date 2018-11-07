4fe24bfbbf55bd97a21676f646d0dba6d1b58556
WangQing0502 <2250070674@qq.com>
2018-11-07 15:41:01 +0800

    xxxxx

---
+++ secondProject/secondProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  secondProject
+//
+//  Created by HBZN on 2018/11/7.
+//  Copyright © 2018年 wangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ secondProject/secondProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  secondProject
+//
+//  Created by HBZN on 2018/11/7.
+//  Copyright © 2018年 wangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
